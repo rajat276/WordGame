58d14214c2c3137efd6ca8ca89e655a96a47ea45
Ghosh <righosh@lm-maa-22004976.paypalcorp.com>
2018-08-04 13:21:08 +0530

    Made it generic for any dic file

--- WordGame/src/bootcamp/fileops/ReadDictionary.java
+++ WordGame/src/bootcamp/fileops/ReadDictionary.java
@@ -5,32 +5,40 @@ import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
 
+class WordList {
+	public ArrayList<String>wordList;
+	public int maxLength;
+	
+	public WordList() {
+		wordList=new ArrayList<>();
+		maxLength=0;
+	}
+}
+
 public class ReadDictionary {
 
 	private final static String DEFAULT_DICTIONARY_FILE="/Users/righosh/Downloads/sowpods.txt";
-	private static int maxLength; 
 
-	private static ArrayList<String>getAllWords(String fileName) throws Exception {
-		ArrayList<String>wordList=new ArrayList<>();
+	private static WordList getAllWords(String fileName) throws Exception {
+		WordList wordList=new WordList();
 		BufferedReader br=new BufferedReader(new FileReader(new File(fileName)));
 		String str;
 		int max=0;
 		while((str=br.readLine())!=null) {
-			wordList.add(str);
-			if(str.length()>max)
-				max=str.length();
+			wordList.wordList.add(str);
+			if(str.length()>wordList.maxLength)
+				wordList.maxLength=str.length();
 		}
-		ReadDictionary.maxLength=max;
 		return wordList;
 	}
 
 	public static ArrayList<ArrayList<String>>getWordList() {
 		try {
-			ArrayList<String>allWords=getAllWords(ReadDictionary.DEFAULT_DICTIONARY_FILE);
+			WordList allWords=getAllWords(ReadDictionary.DEFAULT_DICTIONARY_FILE);
 			ArrayList<ArrayList<String>>list=new ArrayList<>();
-			for(int i=0;i<=ReadDictionary.maxLength+1;i++)
+			for(int i=0;i<=allWords.maxLength+1;i++)
 				list.add(new ArrayList<String>());
-			for(String str:allWords)
+			for(String str:allWords.wordList)
 				list.get(str.length()).add(str);
 			return list;
 		}
@@ -42,11 +50,11 @@ public class ReadDictionary {
 
 	public static ArrayList<ArrayList<String>>getWordList(String fileName) {
 		try {
-			ArrayList<String>allWords=getAllWords(fileName);
+			WordList allWords=getAllWords(fileName);
 			ArrayList<ArrayList<String>>list=new ArrayList<>();
-			for(int i=0;i<=ReadDictionary.maxLength+1;i++)
+			for(int i=0;i<=allWords.maxLength+1;i++)
 				list.add(new ArrayList<String>());
-			for(String str:allWords)
+			for(String str:allWords.wordList)
 				list.get(str.length()).add(str);
 			return list;
 		}
@@ -56,12 +64,10 @@ public class ReadDictionary {
 		}
 	}
 	
-	
 	/*
+	
 	public static void main(String a[]) {
 		ArrayList<ArrayList<String>>list=getWordList();
-		if(list==null)
-			System.out.println("Hello");
 		for(int i=0;i<=10;i++)
 			System.out.println(list.get(4).get(i)+" | "+list.get(5).get(i));
 	}
